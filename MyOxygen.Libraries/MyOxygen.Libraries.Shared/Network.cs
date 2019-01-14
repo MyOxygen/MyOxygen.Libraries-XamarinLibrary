dd2f1a58e58e8e74d7cb04a136d49fa237a0d81a
Tom <tom@myoxygen.co.uk>
2019-01-14 13:39:13 +0000

    Initial commit.
    Created library for Dialogs using ACR.UserDialogs, and networking using Xam.Plugins.Connectivity.

---
+++ MyOxygen.Libraries/MyOxygen.Libraries.Shared/Network.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Linq;
+using Plugin.Connectivity;
+using Plugin.Connectivity.Abstractions;
+
+namespace MyOxygen.Libraries
+{
+    public static class Network
+    {
+        /// <summary>
+        /// Check if there is a connection by WiFi or Mobile Data.
+        /// </summary>
+        /// <returns>True for either WiFi or Mobile Data, otherwise false.</returns>
+        public static bool IsConnectionWifiOrCellular()
+        {
+            return IsConnectionWifi() || IsConnectionCellular();
+        }
+
+
+        /// <summary>
+        /// Checks if there is a connection by WiFi.
+        /// </summary>
+        /// <returns>True for yes, otherwise false.</returns>
+        public static bool IsConnectionWifi()
+        {
+            var profiles = CrossConnectivity.Current.ConnectionTypes;
+            return profiles.Contains(ConnectionType.WiFi);
+        }
+
+
+        /// <summary>
+        /// Checks if there is a connection by Mobile Data.
+        /// </summary>
+        /// <returns>True for yes, otherwise false.</returns>
+        public static bool IsConnectionCellular()
+        {
+            var profiles = CrossConnectivity.Current.ConnectionTypes;
+            return profiles.Contains(ConnectionType.Cellular);
+        }
+
+
+        /// <summary>
+        /// Makes sure all special characters are escaped.
+        /// </summary>
+        /// <param name="urlEntry">Entry to encode for the URL.</param>
+        /// <returns>Encoded URL entry.</returns>
+        public static string EncodeForUrl(string urlEntry)
+        {
+            // https://stackoverflow.com/a/34189188/3872145
+            return Uri.EscapeDataString(urlEntry);
+        }
+
+
+        /// <summary>
+        /// Sets the connection changed event.
+        /// </summary>
+        /// <param name="connectivityChangedEventHandler">Connectivity changed event handler.</param>
+        public static void SetOnConnectionChangedEvent(ConnectivityChangedEventHandler connectivityChangedEventHandler)
+        {
+            CrossConnectivity.Current.ConnectivityChanged -= connectivityChangedEventHandler;
+            CrossConnectivity.Current.ConnectivityChanged += connectivityChangedEventHandler;
+        }
+
+
+        /// <summary>
+        /// Sets the connection type changed event.
+        /// </summary>
+        /// <param name="connectivityTypeChangedEventHandler">Connectivity changed event handler.</param>
+        public static void SetOnConnectionTypeChangedEvent(ConnectivityTypeChangedEventHandler connectivityTypeChangedEventHandler)
+        {
+            CrossConnectivity.Current.ConnectivityTypeChanged -= connectivityTypeChangedEventHandler;
+            CrossConnectivity.Current.ConnectivityTypeChanged += connectivityTypeChangedEventHandler;
+        }
+    }
+}
---
+++ MyOxygen.Libraries/MyOxygen.Libraries.Shared/SimpleDialogs.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Threading.Tasks;
+using Acr.UserDialogs;
+using Xamarin.Forms;
+
+namespace MyOxygen.Libraries
+{
+    public static class SimpleDialogs
+    {
+        private static IProgressDialog loadingDialog;
+        private static IDisposable standardDialog;
+        private static IDisposable entryDialog;
+
+
+        /// <summary>
+        /// Displays a standard message box.
+        /// </summary>
+        /// <param name="title">Title of the dialog.</param>
+        /// <param name="text">Content of the dialog box.</param>
+        /// <param name="okText">Text to close the dialog (default: "OK").</param>
+        public static void InfoDialog(string title, string text, string okText = "OK")
+        {
+            Device.BeginInvokeOnMainThread(() =>
+                standardDialog = UserDialogs.Instance.Alert(title: title, message: text, okText: okText));
+        }
+
+
+        /// <summary>
+        /// Displays a generic loading dialog.
+        /// </summary>
+        /// <param name="text">Text to display to the user.</param>
+        public static void LoadingDialogShow(string text)
+        {
+            if (loadingDialog == null)
+            {
+                Device.BeginInvokeOnMainThread(() =>
+                    loadingDialog = UserDialogs.Instance.Loading(
+                       title: text,
+                       show: true));
+            }
+            else if (!loadingDialog.IsShowing)
+            {
+                loadingDialog.Title = text;
+                loadingDialog.Show();
+            }
+            else
+            {
+                Device.BeginInvokeOnMainThread(() => loadingDialog.Title = text);
+            }
+        }
+
+
+        /// <summary>
+        /// Closes the loading dialog if it is open.
+        /// </summary>
+        public static void LoadingDialogDismiss()
+        {
+            Device.BeginInvokeOnMainThread(() =>
+            {
+                if ((loadingDialog != null) && (loadingDialog.IsShowing))
+                {
+                    loadingDialog.Hide();
+                }
+            });
+        }
+
+
+        /// <summary>
+        /// Displays a generic entry dialog, where the user can type data in.
+        /// </summary>
+        /// <param name="title">Dialog title.</param>
+        /// <param name="message">Dialog display message.</param>
+        /// <param name="entryContent">Entry content.</param>
+        /// <param name="onEntryCompletionHandler">On entry completion handler.</param>
+        public static void EntryDialog(string title, string message, string entryContent, Action<string> onEntryCompletionHandler)
+        {
+            EntryDialog(title, message, entryContent, onEntryCompletionHandler, Strings.OkString, Strings.CancelString);
+        }
+
+
+        /// <summary>
+        /// Displays an entry dialog, where the user can type data in.
+        /// </summary>
+        /// <param name="title">Dialog title.</param>
+        /// <param name="message">Dialog display message.</param>
+        /// <param name="entryContent">Entry content.</param>
+        /// <param name="onEntryCompletionHandler">On entry completion handler.</param>
+        /// <param name="completeButtonText">Complete button text.</param>
+        /// <param name="cancelButtonText">Cancel button text.</param>
+        public static void EntryDialog(string title, string message, string entryContent, Action<string> onEntryCompletionHandler, string completeButtonText, string cancelButtonText)
+        {
+            PromptConfig promptConfig = new PromptConfig()
+            {
+                Title = title,
+                Text = entryContent,
+                Message = message,
+                CancelText = cancelButtonText,
+                IsCancellable = true,
+                OkText = completeButtonText,
+                OnAction = (result) =>
+                {
+                    if (result.Ok)
+                    {
+                        onEntryCompletionHandler(result.Text);
+                    }
+                }
+            };
+            Device.BeginInvokeOnMainThread(() =>
+                entryDialog = UserDialogs.Instance.Prompt(promptConfig));
+        }
+
+
+        /// <summary>
+        /// Displays a confirmation dialog, prompting the user for a yes/no response.
+        /// </summary>
+        /// <returns>The yes/no response, where <see langword="true"/> is a "Yes" response.</returns>
+        /// <param name="title">Title.</param>
+        /// <param name="message">Message.</param>
+        public static Task<bool> YesNoDialogAsync(string title, string message)
+        {
+            return ConfirmationDialogAsync(title, message, Strings.YesString, Strings.NoString);
+        }
+
+
+        /// <summary>
+        /// Displays a generic confirmation dialog.
+        /// </summary>
+        /// <returns>The user response, where <see langword="true"/> is a positive" response.</returns>
+        /// <param name="title">Title.</param>
+        /// <param name="message">Message.</param>
+        /// <param name="confirmButtonText">Confirmation button text.</param>
+        /// <param name="denyButtonText">Deny button text.</param>
+        public static Task<bool> ConfirmationDialogAsync(string title, string message, string confirmButtonText, string denyButtonText)
+        {
+            ConfirmConfig confirmConfig = new ConfirmConfig()
+            {
+                Title = title,
+                Message = message,
+                OkText = confirmButtonText,
+                CancelText = denyButtonText
+            };
+
+            return UserDialogs.Instance.ConfirmAsync(confirmConfig);
+        }
+
+
+        public static void GenericSystemErrorDialog(string message)
+        {
+            InfoDialog(Strings.SystemErrorString, message);
+        }
+
+        public static void GenericUserErrorDialog(string message)
+        {
+            InfoDialog(Strings.UserErrorString, message);
+        }
+
+        public static void GenericSuccessDialog(string message)
+        {
+            InfoDialog(Strings.SuccessString, message);
+        }
+
+        public static void GenericWarningDialog(string message)
+        {
+            InfoDialog(Strings.WarningString, message);
+        }
+
+        public static void GenericInformationDialog(string message)
+        {
+            InfoDialog(Strings.InformationString, message);
+        }
+
+        public static void GenericHelpDialog(string message)
+        {
+            InfoDialog(Strings.HelpString, message);
+        }
+
+
+        public static class Strings
+        {
+            public static string OkString = "OK";
+            public static string CancelString = "Cancel";
+            public static string YesString = "Yes";
+            public static string NoString = "No";
+            public static string SystemErrorString = "Error";
+            public static string UserErrorString = "Oops!";
+            public static string SuccessString = "Success";
+            public static string WarningString = "Warning";
+            public static string InformationString = "Information";
+            public static string HelpString = "Help";
+        }
+    }
+}
